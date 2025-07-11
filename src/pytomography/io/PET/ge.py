240b335775e6b6621b443b2fab23f22eea43b489
James <jdefowler@gmail.com>
2025-07-10 17:15:03 -0700

    cleanup

--- src/pytomography/io/PET/ge.py
+++ src/pytomography/io/PET/ge.py
@@ -4,8 +4,7 @@ import pytomography
 import torch
 import numpy as np
 from pytomography.utils import get_1d_gaussian_kernel
-from .shared import get_axial_trans_ids_from_info, get_detector_ids_from_trans_axial_ids
-
+from .shared import get_scanner_LUT, get_axial_trans_ids_from_info, get_detector_ids_from_trans_axial_ids
 
 def compute_sinogram_ids(info: dict):
     """Computes the sinogram detector IDs in the correct order and shape.
@@ -23,6 +22,7 @@ def compute_sinogram_ids(info: dict):
     min_crystal_difference = min_sector_difference * nr_modules_trans * nr_crystals_trans
     angular_size = int(nr_crystals_per_ring / 2)
     radial_size = int(nr_crystals_per_ring - 2 * min_crystal_difference - 1)
+    distance_crystal_id_0_to_first_sector_center = (nr_modules_trans * nr_crystals_trans) / 2
 
     angular = torch.arange(angular_size).unsqueeze(1)  # shape (A, 1)
     radial = torch.arange(radial_size).unsqueeze(0)   # shape (1, R)
@@ -32,8 +32,8 @@ def compute_sinogram_ids(info: dict):
     radial = radial.expand(angular_size, radial_size)
 
     # Compute crystal indices
-    id_a = (3 * nr_crystals_per_ring / 4) + (min_crystal_difference / 2) + torch.floor(radial / 2) - angular
-    id_b = (3 * nr_crystals_per_ring / 4) - (min_crystal_difference / 2) - torch.floor((radial + 1) / 2) - angular
+    id_a = (3 * nr_crystals_per_ring / 4) + (min_crystal_difference / 2) + torch.floor(radial / 2) + distance_crystal_id_0_to_first_sector_center - angular
+    id_b = (3 * nr_crystals_per_ring / 4) - (min_crystal_difference / 2) - torch.floor((radial + 1) / 2) + distance_crystal_id_0_to_first_sector_center - angular
 
     # Wrap around with modulo
     id_a = id_a.long() % nr_crystals_per_ring
@@ -41,7 +41,6 @@ def compute_sinogram_ids(info: dict):
 
     sinogram_detector_ids = torch.stack([id_a, id_b], dim=-1)
 
-    ring_coordinates = np.zeros((nr_rings * nr_rings - nr_rings+1, 2), dtype=np.float32)
     ring1_list = []
     ring2_list = []
     upper_ring_idx = nr_rings -1
@@ -79,7 +78,6 @@ def sinogram_to_spatial(info: dict) -> Sequence[torch.Tensor]:
     Returns:
         Sequence[torch.Tensor]: Two tensors yielding spatial coordinates
     """
-
     scanner_lut = get_scanner_LUT(info)
     nr_sectors_trans, nr_sectors_axial, nr_modules_axial, nr_modules_trans, nr_crystals_trans, nr_crystals_axial = info['rsectorTransNr'], info['rsectorAxialNr'], info['moduleAxialNr'], info['moduleTransNr'], info['crystalTransNr'], info['crystalAxialNr']
     nr_rings = nr_sectors_axial * nr_modules_axial * nr_crystals_axial
@@ -100,44 +98,6 @@ def sinogram_to_spatial(info: dict) -> Sequence[torch.Tensor]:
     ring_coordinates[:, 1] = scanner_lut[info['NrCrystalsPerRing']*(ring_ids[:,1]-1), 2]
 
     return torch.tensor(detector_coordinates).to(torch.float32), torch.tensor(ring_coordinates).to(torch.float32)
-
-def get_scanner_LUT(info: dict):
-    """Obtains scanner lookup table (gives x/y/z coordinates for each detector ID)
-
-    Args:
-        info (dict): PET geometry information dictionary
-
-    Returns:
-        torch.Tensor[N_detectors, 3]: Lookup table
-    """
-    ids_trans_crystal, ids_axial_crystal, ids_trans_submodule, ids_axial_submodule, ids_trans_module, ids_axial_module, ids_trans_rsector, ids_axial_rsector = get_axial_trans_ids_from_info(info)
-    ids_detector = get_detector_ids_from_trans_axial_ids(ids_trans_crystal, ids_trans_submodule, ids_trans_module, ids_trans_rsector, ids_axial_crystal, ids_axial_submodule, ids_axial_module, ids_axial_rsector, info)
-    Z_modules = ids_axial_module * info['moduleAxialSpacing'] - (info['moduleAxialNr']-1) * info['moduleAxialSpacing'] / 2
-    Z_submodules = Z_modules + ids_axial_submodule * info['submoduleAxialSpacing'] - (info['submoduleAxialNr']-1) * info['submoduleAxialSpacing'] / 2
-    Z_crystals = Z_submodules + ids_axial_crystal * info['crystalAxialSpacing'] - (info['crystalAxialNr']-1) * info['crystalAxialSpacing'] / 2
-    # Get X/Y position of crystals
-    # Start by getting X/Y position inside a submodule aligned with the center of the scanner
-    Y_modules = ids_trans_module * info['moduleTransSpacing'] - (info['moduleTransNr']-1) * info['moduleTransSpacing'] / 2
-    Y_submodules = Y_modules + ids_trans_submodule * info['submoduleTransSpacing'] - (info['submoduleTransNr']-1) * info['submoduleTransSpacing'] / 2
-    Y_crystals = Y_submodules +ids_trans_crystal * info['crystalTransSpacing'] - (info['crystalTransNr']-1) * info['crystalTransSpacing'] / 2
-    X_crystals = info['radius'] * torch.ones(len(Y_crystals))
-    # Now apply rotation based on angle of the rsector
-    angle_rsector = ids_trans_rsector / info['rsectorTransNr'] * 2 * np.pi
-    rotation_matrices = torch.stack([
-        torch.cos(angle_rsector),
-        -torch.sin(angle_rsector),
-        torch.sin(angle_rsector),
-        torch.cos(angle_rsector)], dim=1).view(-1, 2, 2)
-    XY_crystals = torch.vstack([X_crystals, Y_crystals])
-    XY_crystals = torch.einsum('ijk,ki->ij', rotation_matrices, XY_crystals)
-    if info['firstCrystalAxis'] == 0: # crystal with index 0 along x axis
-        XY_crystals = XY_crystals.flip(dims=(1,))
-    # Stack all together (for some reason Z needs to be negative?)
-    XYZ_crystals = torch.vstack([XY_crystals.T, -Z_crystals.unsqueeze(0)]).T
-    # Now sort scanner LUT by ids_detector order
-    XYZ_crystals = XYZ_crystals[torch.argsort(ids_detector)]
-    
-    return XYZ_crystals
    
 def crystal_efficienies_to_sinogram(
     crystal_efficiencies: torch.Tensor,
@@ -177,7 +137,6 @@ def crystal_efficienies_to_sinogram(
 
     return torch.tensor(crystal_sino)
 
-
 def singles_to_randoms_sinogram(
     singles_rate: torch.Tensor,
     coincidence_window: np.float,
@@ -215,88 +174,3 @@ def singles_to_randoms_sinogram(
     randoms_rate_sino[:,:,:] = 2 * coincidence_window * singles_rate[id_a,ring_id_a-1] * singles_rate[id_b,ring_id_b-1]
 
     return torch.tensor(randoms_rate_sino)
-
-def get_detector_ids_from_trans_axial_ids(
-    ids_trans_crystal: torch.Tensor,
-    ids_trans_submodule: torch.Tensor,
-    ids_trans_module: torch.Tensor,
-    ids_trans_rsector: torch.Tensor,
-    ids_axial_crystal: torch.Tensor,
-    ids_axial_submodule: torch.Tensor,
-    ids_axial_module: torch.Tensor,
-    ids_axial_rsector: torch.Tensor,
-    info: dict
-    ) -> torch.Tensor:
-    """Obtain detector IDs from individual part IDs
-
-    Args:
-        ids_trans_crystal (torch.Tensor): Transaxial crystal IDs
-        ids_trans_submodule (torch.Tensor): Transaxial submodule IDs
-        ids_trans_module (torch.Tensor): Transaxial module IDs
-        ids_trans_rsector (torch.Tensor): Transaxial rsector IDs
-        ids_axial_crystal (torch.Tensor): Axial crystal IDs
-        ids_axial_submodule (torch.Tensor): Axial submodule IDs 
-        ids_axial_module (torch.Tensor): Axial module IDs
-        ids_axial_rsector (torch.Tensor): Axial rsector IDs 
-        info (dict): PET geometry information dictionary
-
-    Returns:
-        torch.Tensor: Tensor containing (spatial) detector IDs
-    """
-    ids_ring = ids_axial_crystal +\
-        ids_axial_submodule * info['crystalAxialNr'] +\
-        ids_axial_module * info['crystalAxialNr'] * info['submoduleAxialNr'] +\
-        ids_axial_rsector * info['crystalAxialNr'] * info['submoduleAxialNr'] * info['moduleAxialNr']
-    ids_within_ring = ids_trans_crystal +\
-        ids_trans_submodule * info['crystalTransNr'] +\
-        ids_trans_module * info['crystalTransNr'] * info['submoduleTransNr'] +\
-        ids_trans_rsector * info['crystalTransNr'] * info['submoduleTransNr'] * info['moduleTransNr']
-    nb_crystal_per_ring = info['crystalTransNr'] * info['moduleTransNr'] * info['submoduleTransNr'] * info['rsectorTransNr']
-    ids_detector = ids_ring * nb_crystal_per_ring + ids_within_ring
-    return ids_detector
-
-def get_axial_trans_ids_from_info(
-    info: dict,
-    return_combinations: bool = False,
-    sort_by_detector_ids: bool = False
-    ):
-    """Get axial and transaxial IDs corresponding to each crystal in the scanner
-
-    Args:
-        info (dict): PET geometry information dictionary
-        return_combinations (bool, optional): Whether or not to return all possible combinations (crystal pairs). Defaults to False.
-        sort_by_detector_ids (bool, optional): Whether or not to sort by increasing detector IDs. Defaults to False.
-
-    Returns:
-        Sequence[torch.Tensor]: IDs corresponding to axial/transaxial components of each part
-    """
-    ids_trans_crystal = torch.arange(0, info['crystalTransNr'])
-    ids_axial_crystal = torch.arange(0, info['crystalAxialNr'])
-    ids_trans_submodule = torch.arange(0, info['submoduleTransNr'])
-    ids_axial_submodule = torch.arange(0, info['submoduleAxialNr'])
-    ids_trans_module = torch.arange(0, info['moduleTransNr'])
-    ids_axial_module = torch.arange(0, info['moduleAxialNr'])
-    ids_trans_rsector = torch.arange(0, info['rsectorTransNr'])
-    ids_axial_rsector = torch.arange(0, info['rsectorAxialNr'])
-    ids_trans_crystal, ids_axial_crystal, ids_trans_submodule, ids_axial_submodule, ids_trans_module, ids_axial_module, ids_trans_rsector, ids_axial_rsector = torch.cartesian_prod(ids_trans_crystal, ids_axial_crystal, ids_trans_submodule, ids_axial_submodule, ids_trans_module, ids_axial_module, ids_trans_rsector, ids_axial_rsector).T
-    if sort_by_detector_ids:
-        ids_detector = get_detector_ids_from_trans_axial_ids(ids_trans_crystal, ids_trans_submodule, ids_trans_module, ids_trans_rsector, ids_axial_crystal, ids_axial_submodule, ids_axial_module, ids_axial_rsector, info)
-        idx_sort = torch.argsort(ids_detector)
-        ids_trans_crystal = ids_trans_crystal[idx_sort]
-        ids_axial_crystal = ids_axial_crystal[idx_sort]
-        ids_trans_submodule = ids_trans_submodule[idx_sort]
-        ids_axial_submodule = ids_axial_submodule[idx_sort]
-        ids_trans_module = ids_trans_module[idx_sort]
-        ids_axial_module = ids_axial_module[idx_sort]
-        ids_trans_rsector = ids_trans_rsector[idx_sort]
-        ids_axial_rsector = ids_axial_rsector[idx_sort]
-    if return_combinations:
-        ids_trans_crystal = torch.combinations(ids_trans_crystal, 2)
-        ids_axial_crystal = torch.combinations(ids_axial_crystal, 2)
-        ids_trans_submodule = torch.combinations(ids_trans_submodule, 2)
-        ids_axial_submodule = torch.combinations(ids_axial_submodule, 2)
-        ids_trans_module = torch.combinations(ids_trans_module, 2)
-        ids_axial_module = torch.combinations(ids_axial_module, 2)
-        ids_trans_rsector = torch.combinations(ids_trans_rsector, 2)
-        ids_axial_rsector = torch.combinations(ids_axial_rsector, 2)
-    return ids_trans_crystal, ids_axial_crystal, ids_trans_submodule, ids_axial_submodule, ids_trans_module, ids_axial_module, ids_trans_rsector, ids_axial_rsector
